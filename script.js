20671f6ba35384f4623b04b11b9be9ec10e34846
Lucas Felipe <lucasfelipe734@gmail.com>
2024-07-27 23:43:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -122,4 +122,36 @@ function enviarDados() {
             alert("Usuário Inválido");
         }
     });
+
+    app.get('/informacoes-boleto/:codigoBarras', async (req, res) => {
+        const codigoBarras = req.params.codigoBarras; // Captura o número do código de barras a partir dos parâmetros da URL
+    
+        const connection = await db.getConnection(); // Use a conexão do módulo de banco de dados
+    
+        try {
+            // Consulta a tabela "boleto" com base no número do código de barras fornecido
+            const [boletoInfo] = await connection.execute('SELECT cnpj, razaoSocial, valorBoleto, bancoBoleto FROM boleto WHERE codigoBarras = ?', [codigoBarras]);
+    
+            if (boletoInfo.length === 0) {
+                throw new Error('Informações de boleto não encontradas para o código de barras fornecido.');
+            }
+    
+            // Retorna as informações do boleto em formato JSON
+            res.json({
+                success: true,
+                cnpj: boletoInfo[0].cnpj,
+                razaoSocial: boletoInfo[0].razaoSocial,
+                valorBoleto: boletoInfo[0].valorBoleto,
+                bancoBoleto: boletoInfo[0].bancoBoleto
+            });
+        } catch (error) {
+            res.json({
+                success: false,
+                error: error.message
+            });
+        } finally {
+            connection.release();
+        }
+    });
+    
 }
